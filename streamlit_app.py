3e29fa0449dedc4f4e6fbbd1edc40570ea92438e
Caio Vianna Gonzalez <112041018+caiovg91@users.noreply.github.com>
2025-04-30 10:56:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen
-        smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/{fruit_chosen.lower()}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
